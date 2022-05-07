17a3a83156b35b2824e8f0517a6535239077cce8
Aaron Contreras <aaronlcaq@gmail.com>
2022-05-07 15:44:51 -0500

    feat(gameboard): build skeleton for Gameboard module
    Creates a Gameboard module that serves as the single gameboard that holds the tic-tac-toe grid.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 console.log('Hello world🌊');
+
+const gameBoard = (() => {
+  const grid = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+
+  return {
+    grid
+  };
+})();
+
+console.log(gameBoard.grid);
